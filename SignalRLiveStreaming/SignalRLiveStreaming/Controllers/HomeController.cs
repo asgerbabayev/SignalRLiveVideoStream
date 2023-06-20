309eef61546edce262c371dc135208e996af7f72
asgerbabayev <asgerbabayev@hotmail.com>
2023-06-21 00:27:26 +0400

    added peer.js

---
+++ SignalRLiveStreaming/SignalRLiveStreaming/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRLiveStreaming.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect($"{Guid.NewGuid()}");
+        }
+        [HttpGet("/{roomId}")]
+        public IActionResult Room(string roomId)
+        {
+            ViewBag.roomId = roomId;
+            return View();
+        }
+    }
+}
---
+++ SignalRLiveStreaming/SignalRLiveStreaming/Hubs/Users.cs
@@ -0,0 +1,6 @@
+﻿namespace SignalRLiveStreaming.Hubs;
+
+public static class Users
+{
+    public static IDictionary<string, string> list = new Dictionary<string, string>();
+}
